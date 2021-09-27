ca50a5ee0c4d76955d9a68383b9ce15142b58828
Alexandre Switalski <alexandre.swit@gmail.com>
2021-09-27 09:52:32 +0200

    feat: add nuxt-vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,57 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-typescript-composition-api',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  modern: process.env.NODE_ENV === 'production',
+  ssr: true,
+  target: 'server',
+
   plugins: [],
 
-  components: [{ path: '~/components', pathPrefix: false, ignore: ['**/*.stories.ts'], extensions: ['vue'] }],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+      ignore: ['**/*.stories.ts'],
+      extensions: ['vue'],
+    },
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxt/typescript-build',
     '@nuxtjs/stylelint-module',
-    '@nuxtjs/composition-api/module'
+    '@nuxtjs/composition-api/module',
+    'nuxt-vite',
   ],
 
+  vite: {
+    ssr: true,
+    build: true,
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-  ],
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa'],
 
   axios: {},
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
